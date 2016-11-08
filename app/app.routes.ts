e524ba9c2524654583aee0f048e783efb10f4ae9
Rebecca Dicharry <rdicharry@gmail.com>
2016-11-07 20:27:29 -0700

    some changes to get it to build.

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,7 +1,7 @@
 import {RouterModule, Routes} from "@angular/router";
 import {SplashComponent} from "./components/splash-component";
 
-
+export const allAppComponents = [SplashComponent];
 
 export const routes: Routes = [
 	{path: "", component: SplashComponent}
